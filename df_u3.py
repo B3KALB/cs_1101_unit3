d3bdb7e18c13205c42fa878fd2f553c0b1b8baa6
B3KALB <B3KALB@gmail.com>
2022-09-19 14:02:15 -0500

    unit3_1101_3versions
    3 versions of unit 3 discussion forum

--- df_u3.py
+++ df_u3.py
@@ -3,6 +3,71 @@ take_a_guess = 13
 # Chained conditionals
 def check_it_out(incoming):
     if incoming == 13:
+        print(f"{incoming}:You nailed it!")
+    elif incoming <= 12:
+        print(f"{incoming}: Bit highter!")
+    else:
+        print(f"{incoming}: Too high!") 
+
+check_it_out(take_a_guess)
+
+# Nested conditionals
+
+def nest_it_deep(incoming):
+    if incoming <= 12:
+        print(f"{incoming}: Bit highter!")
+    else:
+        if incoming >= 14:
+            print(f"{incoming}: Too high!")
+        else:
+            print(f"{incoming}: You nailed it!")
+
+nest_it_deep(take_a_guess)# og version
+# 
+# 
+take_a_guess_right = 13
+take_a_guess_low = 2
+take_a_guess_high = 19
+
+# Chained conditionals
+def check_it_out(incoming):
+    if incoming == 13:
+        print(f"{incoming}: You nailed it!")
+    elif incoming <= 12:
+        print(f"{incoming}: Bit higher!")
+    else:
+        print(f"{incoming}: Too high!") 
+
+check_it_out(take_a_guess_low)
+check_it_out(take_a_guess_right)
+check_it_out(take_a_guess_high)
+
+# Nested conditionals
+
+def nest_it_deep(incoming):
+    if incoming <= 12:
+        print(f"{incoming}: Bit higher!")
+    else:
+        if incoming >= 14:
+            print(f"{incoming}: Too high!")
+        else:
+            print(f"{incoming}: You nailed it!")
+
+nest_it_deep(take_a_guess_low)
+nest_it_deep(take_a_guess_right)
+nest_it_deep(take_a_guess_high)
+
+# 
+# 
+# 
+# input version
+whole_num = input("Please enter an integer: ")
+
+take_a_guess = int(whole_num)
+
+# Chained conditionals
+def check_it_out(incoming):
+    if incoming == 13:
         print(f"{incoming}: You nailed it!")
     elif incoming <= 12:
         print(f"{incoming}: Bit highter!")

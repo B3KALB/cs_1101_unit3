f1ac467d36a499109e42d55da27183e52b7c04d5
B3KALB <B3KALB@gmail.com>
2022-09-19 12:05:11 -0500

    unit3_DF
    clean up and play around!

--- df_u3.py
+++ df_u3.py
@@ -1,9 +1,9 @@
-take_a_guess = 6
+take_a_guess = 13
 
 # Chained conditionals
 def check_it_out(incoming):
     if incoming == 13:
-        print(f"{incoming}:You nailed it!")
+        print(f"{incoming}: You nailed it!")
     elif incoming <= 12:
         print(f"{incoming}: Bit highter!")
     else:
@@ -20,6 +20,6 @@ def nest_it_deep(incoming):
         if incoming >= 14:
             print(f"{incoming}: Too high!")
         else:
-            print(f"{incoming}:You nailed it!")
+            print(f"{incoming}: You nailed it!")
 
 nest_it_deep(take_a_guess)

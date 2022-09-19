b38478834018687b5fa9aa77eae82a41a0c0d758
B3KALB <B3KALB@gmail.com>
2022-09-19 12:02:35 -0500

    DisussionForumUnit3Pt2
    Nested conditionals
    
    Deeply nested conditionals can become difficult to read. Describe a strategy for avoiding nested conditionals. Give your own example of a nested conditional that can be modified to become a single conditional, and show the equivalent single conditional.

--- df_u3.py
+++ df_u3.py
@@ -10,3 +10,16 @@ def check_it_out(incoming):
         print(f"{incoming}: Too high!") 
 
 check_it_out(take_a_guess)
+
+# Nested conditionals
+
+def nest_it_deep(incoming):
+    if incoming <= 12:
+        print(f"{incoming}: Bit highter!")
+    else:
+        if incoming >= 14:
+            print(f"{incoming}: Too high!")
+        else:
+            print(f"{incoming}:You nailed it!")
+
+nest_it_deep(take_a_guess)
